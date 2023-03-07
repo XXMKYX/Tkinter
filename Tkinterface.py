a6a145159318988a29175cc9f8ecf6d3bcdbf7f8
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2023-03-07 12:20:45 -0600

    Buttons
    Botones y sus configuraciones

--- Tkinterface.py
+++ Tkinterface.py
@@ -9,7 +9,20 @@ root.geometry("500x300") #Dimensiones
 root.resizable(0,0) #Desactivamos opcion para modificar ventana
 root.config(bg="grey",cursor="circle") #color y tipo de cursor
 
-texto = Label(root,text="SCI") #texto
+#BOTONES
+button1 = Button(root,text="Minimizar",command = root.iconify,bg="green") #Minimiza el borton
+button1.pack(side=RIGHT) #Posiciona el boton en la ventana
+#Funcion para imprimir
+def imprimir():
+    print("Printing...")
+    label1 = Label(root,text="Printing...")
+    label1.pack()
+
+button2 = Button(root,text="Print",command = imprimir,bg="pink")
+button2.pack(side=LEFT)
+
+
+texto = Label(root,text="PROOF") #texto
 texto.pack() #Asignacion del texto a la ventana
 
 #Cierre de la ventana hasta que lo indiquemos
