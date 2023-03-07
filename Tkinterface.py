1009ec140e57e9a2882fdd2b39aff4ded1dc821b
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2023-03-07 12:34:44 -0600

    Positions
    Configuracion de posiciones con por matriz GRID o por coordenada PLACE

--- Tkinterface.py
+++ Tkinterface.py
@@ -10,8 +10,10 @@ root.resizable(0,0) #Desactivamos opcion para modificar ventana
 root.config(bg="grey",cursor="circle") #color y tipo de cursor
 
 #BOTONES
-button1 = Button(root,text="Minimizar",command = root.iconify,bg="green") #Minimiza el borton
-button1.pack(side=RIGHT) #Posiciona el boton en la ventana
+button1 = Button(root,text="-",command = root.iconify,bg="green") #Minimiza el borton
+button1.place(x=180,y=50) #Posiciona el boton en la ventana
+etiqueta1 = Label(root,text="Preciona para minimizar: ")
+etiqueta1.place(x=20,y=50)
 #Funcion para imprimir
 def imprimir():
     print("Printing...")
@@ -19,8 +21,15 @@ def imprimir():
     label1.pack()
 
 button2 = Button(root,text="Print",command = imprimir,bg="pink")
-button2.pack(side=LEFT)
+button2.place(x=180,y=100)
+etiqueta2 = Label(root,text="Preciona para imprimir: ")
+etiqueta2.place(x=20,y=100)
 
+#Organizacion por row y column
+# .grid(row=0,column=1)
+
+#Organizacion por coordenadas Place
+# .place(x=90,y=50)
 
 texto = Label(root,text="PROOF") #texto
 texto.pack() #Asignacion del texto a la ventana
