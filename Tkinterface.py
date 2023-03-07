28f2d05f0def073ed9841df0889c71c800f16ffd
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2023-03-07 13:03:40 -0600

    Input
    Entrada de informacion

--- Tkinterface.py
+++ Tkinterface.py
@@ -4,7 +4,7 @@ from tkinter import*
 root = Tk()
 #Personalizacion ventana
 root.title("SCI") #titulo
-root.geometry("500x300") #Dimensiones
+root.geometry("700x500") #Dimensiones
 #root.iconbitmap("SCI.ico") #Icono
 root.resizable(0,0) #Desactivamos opcion para modificar ventana
 root.config(bg="grey",cursor="circle") #color y tipo de cursor
@@ -31,8 +31,27 @@ etiqueta2.place(x=20,y=100)
 #Organizacion por coordenadas Place
 # .place(x=90,y=50)
 
-texto = Label(root,text="PROOF") #texto
-texto.pack() #Asignacion del texto a la ventana
+#Input
+
+name = StringVar()
+name.set("Name")
+saludo = StringVar()
+
+texto1 = Label(root,text="Ingresa tu nombre", bd=4,font="arial 12") #texto
+texto1.place(x=20,y=180)
+input1 = Entry(root,textvariable=name,bd=4,font="arial 12")
+input1.place(x=180,y=180)
+
+def saludos():
+    print("Greatings to " + name.get())
+    saludo.set("Greatings to " + name.get())
+
+    
+
+buttonName = Button(root,text="Saludame!",bd=5,command = saludos,bg="red")
+buttonName.place(x=20,y=220)
+out1 = Entry(root,text="Greatings to " + name.get(),state="disable",textvariable=saludo,bd=4,font="arial 12") #State inmodificable
+out1.place(x=180, y=220)
 
 #Cierre de la ventana hasta que lo indiquemos
 root.mainloop()
